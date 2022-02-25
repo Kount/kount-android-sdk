8d978b9ab8efd5b92669975cfbf5885ff46ab763
Jyothi-HV-r <57260465+Jyothi-HV-r@users.noreply.github.com>
2022-02-25 21:02:07 +0530

    Release-4.2.2 fixed calling completion handler prior sending data to server. (#19)
    Bug Fix: Fixes a race condition where the collection handlers say they have completed prior to transmissions of the collection data to servers.
    
    Any users of 4.2.X should upgrade to this to resolve the possible race condition.

--- CheckoutExample/app/src/main/java/com/kount/checkoutexample/MainActivity.java
+++ CheckoutExample/app/src/main/java/com/kount/checkoutexample/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -18,57 +17,25 @@ import androidx.core.content.ContextCompat;
 import com.kount.api.analytics.AnalyticsCollector;
 
 import java.util.Locale;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
+    final int PERMISSIONS_REQUEST_LOCATION = 0;
     static final int MERCHANT_ID = 999999; // Insert your valid merchant ID
     static final int ENVIRONMENT = AnalyticsCollector.ENVIRONMENT_TEST;//For production need to add AnalyticsCollector.ENVIRONMENT_PRODUCTION
-    static String deviceSessionID = "";
-    private String KEY_UUID = "UUID";
-
+    TextView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Sample");
-        initView();
+        location = (TextView) findViewById(R.id.location);
 
-        //required section
         AnalyticsCollector.setMerchantId(MERCHANT_ID);
-        //end required section
-
         // This turns the alpha collections on(true)/off(false). It defaults to true
         AnalyticsCollector.collectAnalytics(true);
-
-        // For production you need to add AnalyticsCollector.ENVIRONMENT_PRODUCTION
         AnalyticsCollector.setEnvironment(ENVIRONMENT);
 
-        //Optional SessionID section
-        /** If you want to pass in a self generated sessionID(or one given to you by your servers)
-         * you can set it using colde like this. Otherwise the AnalyticsCollector will generate one
-         * for you.Make sure you set session id only one time in a user session.
-         * To do so set your sessionID below.*/
-        if (savedInstanceState == null) {
-            deviceSessionID = UUID.randomUUID().toString();
-            AnalyticsCollector.setSessionId(deviceSessionID);
-        } else {
-            deviceSessionID = savedInstanceState.getString(KEY_UUID);
-            AnalyticsCollector.setSessionId(deviceSessionID);
-        }
-        // END OPTIONAL SESSION_ID SECTION
-
-        //Request location permission for Android 6.0 & above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            requestLocationPermissions(this);
-        else
-            AnalyticsCollector.collectDeviceDataForSession(this);
-
-
-    }
-
-    private void initView() {
         final TextView merchant = (TextView) findViewById(R.id.merchant);
         final TextView environment = (TextView) findViewById(R.id.environment);
         final Button checkoutButton = (Button) findViewById(R.id.checkoutButton);
@@ -92,21 +59,45 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 startActivity(intent);
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check for location permissions so the Data Collector can gather the device location
+            requestLocationPermission(this);
+        } else {
+            location.setText("Allowed");
+            //Calling this will start standard DeviceData Collection
+            AnalyticsCollector.collectDeviceDataForSession(this);
+        }
+        super.onCreate(savedInstanceState);
+
     }
 
-    void requestLocationPermissions(Activity activity) {
-        final TextView location = (TextView) findViewById(R.id.location);
-
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, AnalyticsCollector.REQUEST_PERMISSION_LOCATION);
+    private void requestLocationPermission(Activity activity) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            ) {
+                ActivityCompat.requestPermissions(
+                        activity,
+                        new String[]{(Manifest.permission.ACCESS_FINE_LOCATION)},
+                        AnalyticsCollector.REQUEST_PERMISSION_LOCATION
+                );
             } else {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, AnalyticsCollector.REQUEST_PERMISSION_LOCATION);
+                ActivityCompat.requestPermissions(
+                        activity,
+                        new String[]{(Manifest.permission.ACCESS_FINE_LOCATION)},
+                        AnalyticsCollector.REQUEST_PERMISSION_LOCATION
+                );
             }
         } else {
-            //This block executes when permission is already granted.
-            AnalyticsCollector.collectDeviceDataForSession(activity);
             location.setText("Allowed");
+            AnalyticsCollector.collectDeviceDataForSession(activity);
         }
     }
 
@@ -114,7 +105,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         final TextView location = (TextView) findViewById(R.id.location);
         if (requestCode == AnalyticsCollector.REQUEST_PERMISSION_LOCATION) {
-            //this block executes when a user grant/deny the permission
             AnalyticsCollector.collectDeviceDataForSession(this);
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 location.setText("Allowed");
@@ -122,12 +112,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 location.setText("Denied");
             }
         }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(KEY_UUID, deviceSessionID);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 }
--- CheckoutExample/kotlinexample/src/main/java/com/kount/checkoutexample/kotlin/MainActivity.kt
+++ CheckoutExample/kotlinexample/src/main/java/com/kount/checkoutexample/kotlin/MainActivity.kt
@@ -11,55 +11,27 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.kount.api.analytics.AnalyticsCollector
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
-    private val ENVIRONMENT: Int = AnalyticsCollector.ENVIRONMENT_TEST//For production need to add AnalyticsCollector.ENVIRONMENT_PRODUCTION
-    var deviceSessionID: String? = ""
-    private val KEY_UUID = "UUID"
+    private val PERMISSIONS_REQUEST_LOCATION = 0
+    private val ENVIRONMENT: Int =
+        AnalyticsCollector.ENVIRONMENT_TEST//For production need to add AnalyticsCollector.ENVIRONMENT_PRODUCTION
+
 
     companion object Cons {
         val MERCHANT_ID = 999999 // Insert your valid merchant ID
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Sample"
-        initView()
 
-        //required section
+
         AnalyticsCollector.setMerchantId(MERCHANT_ID)
-        //end required section
-
-        // This turns the alpha collections on(true)/off(false). It defaults to true
+        //This turns the alpha collections on(true)/off(false). It defaults to true
         AnalyticsCollector.collectAnalytics(true)
-
-        // For production you need to add AnalyticsCollector.ENVIRONMENT_PRODUCTION
         AnalyticsCollector.setEnvironment(ENVIRONMENT)
 
-        //Optional SessionID section
-        /** If you want to pass in a self generated sessionID(or one given to you by your servers)
-         * you can set it using colde like this. Otherwise the AnalyticsCollector will generate one
-         * for you.Make sure you set session id only one time in a user session.
-         * To do so set your sessionID below. */
-        if (savedInstanceState == null) {
-            deviceSessionID = UUID.randomUUID().toString()
-            AnalyticsCollector.setSessionId(deviceSessionID!!)
-        } else {
-            deviceSessionID = savedInstanceState.getString(KEY_UUID)
-            AnalyticsCollector.setSessionId(deviceSessionID!!)
-        }
-        // END OPTIONAL SESSION_ID SECTION
-
-        //Request location permission for Android 6.0 & above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-            requestLocationPermissions(this)
-        else
-            AnalyticsCollector.collectDeviceDataForSession(this)
-    }
-
-    private fun initView() {
         merchant.text = "$MERCHANT_ID"
         when (ENVIRONMENT) {
             AnalyticsCollector.ENVIRONMENT_TEST -> environment.text = "Test"
@@ -71,27 +43,53 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         checkoutButton.setOnClickListener {
             startActivity(Intent(this@MainActivity, CollectionActivity::class.java))
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        // Check for location permissions so the Data Collector can gather the device location
+            requestLocationPermission(this) else {
+            location.text = "Allowed"
+
+            //Calling this will start standard DeviceData Collection
+            AnalyticsCollector.collectDeviceDataForSession(this)
+        }
+        super.onCreate(savedInstanceState)
+
     }
 
-    fun requestLocationPermissions(activity: Activity?) {
-        if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(activity, arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION), AnalyticsCollector.REQUEST_PERMISSION_LOCATION)
+    private fun requestLocationPermission(activity: Activity) {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    AnalyticsCollector.REQUEST_PERMISSION_LOCATION
+                )
             } else {
-                ActivityCompat.requestPermissions(activity, arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION), AnalyticsCollector.REQUEST_PERMISSION_LOCATION)
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    AnalyticsCollector.REQUEST_PERMISSION_LOCATION
+                )
             }
         } else {
-            //This block executes when permission is already granted.
-            AnalyticsCollector.collectDeviceDataForSession(activity)
             location.text = "Allowed"
+            AnalyticsCollector.collectDeviceDataForSession(activity)
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == AnalyticsCollector.REQUEST_PERMISSION_LOCATION) {
-            //this block executes when a user grant/deny the permission
             AnalyticsCollector.collectDeviceDataForSession(this)
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 location.text = "Allowed"
@@ -99,10 +97,6 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
                 location.text = "Denied"
             }
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(KEY_UUID, deviceSessionID)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
